b803d74b5cecd4ea1082d6e04cc01a8d36e94683
Magnus Havgry <magnushavgry@gmail.com>
2019-03-04 21:35:53 +0100

    Add linting setup simply extending Airbnb... With one tiny exception!

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
+				use: ['babel-loader', 'eslint-loader'],
 			},
 			{
 				test: /\.css$/,
