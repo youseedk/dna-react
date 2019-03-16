89ec782ef30b60d74b1140dc6d742249395c1da0
Magnus Havgry <magnushavgry@gmail.com>
2019-03-16 15:56:44 +0100

    Add UI icons from YouSee DNA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
+// Icons
+import iconArrowDown from '@youseedk/dna/svg/ui-icons/arrow-down.svg'
+import iconArrowLeft from '@youseedk/dna/svg/ui-icons/arrow-left.svg'
+import iconArrowRight from '@youseedk/dna/svg/ui-icons/arrow-right.svg'
+import iconArrowUp from '@youseedk/dna/svg/ui-icons/arrow-up.svg'
+import iconCheckMark from '@youseedk/dna/svg/ui-icons/check-mark.svg'
+import iconErase from '@youseedk/dna/svg/ui-icons/erase.svg'
+import iconExclamationMark from '@youseedk/dna/svg/ui-icons/exclamation-mark.svg'
+import iconEyeClosed from '@youseedk/dna/svg/ui-icons/eye-closed.svg'
+import iconEyeOpen from '@youseedk/dna/svg/ui-icons/eye-open.svg'
+import iconLogo from '@youseedk/dna/svg/ui-icons/logo.svg'
+import iconMultiplication from '@youseedk/dna/svg/ui-icons/multiplication.svg'
+import iconPlus from '@youseedk/dna/svg/ui-icons/plus.svg'
+
+// Components
 import Button from './Button'
 import Splash from './Splash'
 
 export {
   Button,
   Splash,
+  iconArrowDown,
+  iconArrowLeft,
+  iconArrowRight,
+  iconArrowUp,
+  iconCheckMark,
+  iconErase,
+  iconExclamationMark,
+  iconEyeClosed,
+  iconEyeOpen,
+  iconLogo,
+  iconMultiplication,
+  iconPlus,
 }
