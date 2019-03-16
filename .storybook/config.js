dd54ca867303b9a6c6b4d67633f2cf3d09c6458c
Magnus Havgry <magnushavgry@gmail.com>
2019-03-16 16:54:57 +0100

    Remove Storybook `addon-centered`
    It doesn't play nice with the `block` display of the Button component and creates confusion

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
-import centered from '@storybook/addon-centered/react'
 import { withTests } from '@storybook/addon-jest'
 
 import results from '../.jest-test-results.json'
@@ -8,7 +7,6 @@ import results from '../.jest-test-results.json'
 const req = require.context('../src', true, /[^/]+\/stories.jsx$/)
 
 addDecorator(withKnobs)
-addDecorator(centered)
 addDecorator(withTests({
   results,
   filesExt: '.test.jsx',
