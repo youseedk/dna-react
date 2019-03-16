a7ad91009c24abb8f724297a70f3eba84e9924b2
Magnus Havgry <magnushavgry@gmail.com>
2019-03-16 16:49:32 +0100

    Override Storybook's SVG file loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,28 @@
+const path = require('path')
+
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /stories.jsx/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre',
-  })
+  // Modify Storybook's file-loader rule to avoid conflicts with svgr
+  const fileLoaderRule = config.module.rules.find(rule => rule.test.test('.svg'))
+  fileLoaderRule.exclude = path.resolve(__dirname, '../node_modules/@youseedk')
+
+  const newRules = [
+    {
+      test: /stories.jsx/,
+      loaders: [require.resolve('@storybook/addon-storysource/loader')],
+      enforce: 'pre',
+    },
+    {
+      test: /\.svg$/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+        },
+      }],
+    },
+  ]
+
+  config.module.rules.push(...newRules)
 
   return config
 }
