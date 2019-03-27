2f513748a0e19fea3c8fea2d85da37bcfae52641
Magnus Havgry <magnushavgry@gmail.com>
2019-03-27 22:22:04 +0100

    Allow importing as CommonJS modules

--- tools/build.js
+++ tools/build.js
@@ -4,18 +4,24 @@ const execa = require('execa')
 
 const srcRoot = path.join(__dirname, '../src')
 const libRoot = path.join(__dirname, '../lib')
+const esRoot = path.join(libRoot, 'es')
 
 const clean = () => fse.existsSync(libRoot) && fse.removeSync(libRoot)
 
 const shell = cmd => execa.shell(cmd, { stdio: ['pipe', 'pipe', 'inherit'] })
 
+const buildLib = async () => {
+  await shell(`npx babel ${srcRoot} --out-dir ${libRoot} --env-name "lib" --copy-files`)
+}
+
 const buildEsm = async () => {
-  await shell(`npx babel ${srcRoot} --out-dir ${libRoot} --env-name "esm" --copy-files`)
+  await shell(`npx babel ${srcRoot} --out-dir ${esRoot} --env-name "esm" --copy-files`)
 }
 
 clean()
 
 Promise.all([
+  buildLib(),
   buildEsm(),
 ]).catch((err) => {
   if (err) {
