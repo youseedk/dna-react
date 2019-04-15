28ce16a411546858068b83e1649d8ccbc0d9da14
Magnus Havgry <magnushavgry@gmail.com>
2019-04-15 16:48:46 +0200

    Move dummy icon to separate file

--- src/Button/Button.test.jsx
+++ src/Button/Button.test.jsx
@@ -2,21 +2,7 @@ import React from 'react'
 import { shallow, mount as mountComponent } from 'enzyme'
 
 import Button from './Button'
-
-const dummyIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-  >
-    <rect
-      x="10"
-      y="10"
-      height="100"
-      width="100"
-      style={{ stroke: '#ff0000', fill: '#0000ff' }}
-    />
-  </svg>
-)
+import dummyIcon from '../utils/dummyIcon'
 
 const defaultLabel = 'Click me!'
 const getButton = props => (
---
+++ src/utils/dummyIcon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const dummyIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <rect
+      x="10"
+      y="10"
+      height="100"
+      width="100"
+      style={{ stroke: '#ff0000', fill: '#0000ff' }}
+    />
+  </svg>
+)
+
+export default dummyIcon
